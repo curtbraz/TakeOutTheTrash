c9ee8fbb42b867da9444a8407f0f51600b09770b
Curtis Brazzell <curtbraz@gmail.com>
2016-03-14 13:32:31 -0400

    Added files via upload

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# TakeOutTheTrash
+# TakeOutTrash
+This Script Notifies You on Trash Day if You Forgot the Trash, Before it's Too Late!  I work late hours and if I don't remember to take the trash out on Thursday evening before I go to sleep, I'll certainly miss it on Friday mornings when the garbage truck comes.  I don't like to be woken up with a reminder on Friday mornings if I did remember the trash, so I wrote this. :)
+
+The script leverages an IP Camera to take an image of the location where you put your trash can.  On a schedule it takes an image a day before trash day, and on trash day before the garbage truck comes.  It does this so lighting conditions outside should be relatively similar.  The script then crops the images down to see just the can location and calculates the percentage difference between the contrast in the two images.  If the contrast is over a specific threshold you can set, it will notify you that you forgot the trash!  You should set this up to give you enough time to take out the trash if you do get a notification.  Consider setting up a special notification tone for these messages on your phone so it wakes you up!  
+
+# Requirements
+  Linux Server (or RaspberryPi-like-device)
+  
+  IP Camera
+  
+  Trash Can :)
+  
+# Instructions
+  1) Set up your IP Camera in a stationary location to take an image of your trash can's position on a schedule, one hour before the trash is taken on trash day and another 24 hours before that.
+  
+  2) Have the IP Camera save these images to your Linux Server, via FTP or whatever method you prefer.
+  
+  3) Clone this repo to your Linux Server.
+  
+  4) Configure SMTP settings in trash.php so that email notifications will work. (you could use another method, like an SMS API for notifications)
+  
+  5) Configure the crop settings in trash.php to narrow down on the trash can.
+  
+  6) Set up a cron job on your Linux Server to execute the script an hour or so before trash day and enjoy the automation!
+
+You may need to play around with the threshold settings.  Everyone has different looking trash cans!  I included the cropped images with and without a can for comparison.
---
+++ trash.php
@@ -0,0 +1,84 @@
+<?php 
+// BRINGS IN CLASS TO COMPARE FINAL IMAGE % DIFFERENCES
+
+include('img_class.php');
+
+// SET THE CROP FOR THE CAMERA IMAGE TO INCLUDE ONLY TRASH CAN
+
+// The Width of the crop
+$w1 = '76';
+// The Height of the crop
+$h1 = '61';
+// The X coordinate of the cropped region's top left corner
+$l1 = '212';
+// The Y coordinate of the cropped region's top left corner
+$t1 = '225';
+
+// CROPS CAMERA IMAGE FOR TRASH DAY
+
+// Grabs the most recent image (should be image with trash can, unless forgotten)
+$filename = shell_exec('ls -t *.jpg | head -1');
+
+$filename = str_replace("\n", '', $filename); // remove new lines
+$filename = str_replace("\r", '', $filename); // remove carriage returns
+
+$thumb = new Imagick($filename);
+$thumb->cropImage($w1,$h1,$l1,$t1);
+$thumb->writeImage('trashday.jpg');
+
+// Grabs yesterday's image (should be image without trash can, unless left out all week)
+$filename = shell_exec('ls -t *.jpg | tail -n1');
+
+$filename = str_replace("\n", '', $filename); // remove new lines
+$filename = str_replace("\r", '', $filename); // remove carriage returns
+
+$thumb = new Imagick($filename);
+$thumb->cropImage($w1,$h1,$l1,$t1);
+$thumb->writeImage('nottrashday.jpg');
+
+// CALCULATE DIFFERENCE BETWEEN BOTH CROPPED IMAGES
+
+$diff = new imagediff('trashday.jpg', 'nottrashday.jpg');
+$percentdiff = ($diff->diff() * 100 );
+
+// Remove all images so the process can repeat next week
+shell_exec('rm *.jpg');
+
+// SEND AN EMAIL IF THE TRASH HAS NOT BEEN TAKEN OUT
+
+// If the percentage difference between the two images is over 70, send an email notification
+if($percentdiff >= 70){
+
+// Include PHP Mailer class
+require_once "Mail-1.3.0/Mail.php";
+
+// Put your email information here
+$from = '<SENDER@gmail.com>';
+$to = '<RECIPIENT@gmail.com>';
+$subject = 'You Forgot to Take Out the Trash!';
+$body = "DOH!  Hurry up and take out the trash before it's too late!  (The difference in images is ".$percentdiff."%)";
+
+$headers = array(
+    'From' => $from,
+    'To' => $to,
+    'Subject' => $subject
+);
+
+// Gmail smtp settings
+$smtp = Mail::factory('smtp', array(
+        'host' => 'ssl://smtp.gmail.com',
+        'port' => '465',
+        'auth' => true,
+        'username' => 'USER@gmail.com',
+        'password' => 'PASSWORD'
+    ));
+
+$mail = $smtp->send($to, $headers, $body);
+
+if (PEAR::isError($mail)) {
+    echo('<p>' . $mail->getMessage() . '</p>');
+}
+
+}
+
+?> 
